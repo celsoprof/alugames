60c74d03e840f5cd9667cbc6fefc10c3077d0059
Prof. Celso Furtado <celso@celso.dev.br>
2024-06-17 07:36:37 -0300

    URL do servidor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ fun main() {
 
     val request = HttpRequest
         .newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146")) // URL do servidor
         .build()
 
     val response = client
